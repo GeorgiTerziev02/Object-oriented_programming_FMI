84ae205f9c0c6cf8a3d519c3e0fa3a3dcded40cf
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-02-23 15:20:43 +0200

    Update StableCountingSort.cpp

--- Sem. 01/Bonus/StableCountingSort.cpp
+++ Sem. 01/Bonus/StableCountingSort.cpp
@@ -3,11 +3,13 @@
 void stableCountingSort(char* arr, size_t length) {
 	size_t arrLength = strlen(arr);
 	char* copy = new char[arrLength];
+	// copy the original
 	for (size_t i = 0; i < arrLength; i++) {
 		copy[i] = arr[i];
 	}
 	
 	int counter[256] = { 0 };
+	// count every char
 	for (size_t i = 0; i < length; i++) {
 		counter[arr[i]]++;
 	}
@@ -18,6 +20,8 @@ void stableCountingSort(char* arr, size_t length) {
 		counter[i] += counter[i - 1];
 	}
 
+	// put the characters in the initial array sorted
+	// started backwards so that the sorting is stable
 	for (int i = arrLength - 1; i >= 0; i--) {
 		arr[--counter[copy[i]]] = copy[i];
 	}
