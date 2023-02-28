8ece3445c8e53b032d2d40ffc63891ffb5045354
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-02-28 21:53:34 +0200

    Update Task08.cpp

--- Sem. 01/Pract. 01/Task08.cpp
+++ Sem. 01/Pract. 01/Task08.cpp
@@ -3,39 +3,30 @@
 const int MAX_SIZE_NAME = 30;
 const int MAX_GROUP_SIZE = 30;
 
-struct Student
-{
-	double grade;
-	unsigned int FN;
+struct Student {
 	char name[MAX_SIZE_NAME + 1];
+	unsigned int fn;
+	double grade;
 };
 
-struct StudentGroup
-{
-	Student groupArr[MAX_GROUP_SIZE];
-	double averageGrade;
+struct StudentGroup {
 	int amountOfStudents;
+	double averageGrade;
+	Student groupArr[MAX_GROUP_SIZE];
 };
 
-double findSum(const Student* arr, int numberOfStudents)
-{
+double findSum(const Student* arr, int numberOfStudents) {
 	double sum = 0;
-
-	for (int i = 0; i < numberOfStudents; i++)
-	{
+	for (int i = 0; i < numberOfStudents; i++) {
 		sum += arr[i].grade;
 	}
 
 	return sum;
 }
 
-void initGroupOfStudents(StudentGroup& group)
-{
-	double sumOfGrades;
-
-	for (int i = 0; i < group.amountOfStudents; i++)
-	{
-		std::cin >> group.groupArr[i].FN;
+void initGroupOfStudents(StudentGroup& group) {
+	for (int i = 0; i < group.amountOfStudents; i++) {
+		std::cin >> group.groupArr[i].fn;
 		std::cin >> group.groupArr[i].name;
 		std::cin.ignore();
 		std::cin >> group.groupArr[i].grade;
@@ -44,14 +35,11 @@ void initGroupOfStudents(StudentGroup& group)
 	group.averageGrade = findSum(group.groupArr, group.amountOfStudents) / group.amountOfStudents;
 }
 
-int numberOfStudentsTakingScholarship(StudentGroup& group, const double minGrade)
-{
-	int studentCounter=0;
+int numberOfStudentsTakingScholarship(const StudentGroup& group, const double minGrade) {
+	int studentCounter = 0;
 
-	for (int i = 0; i < group.amountOfStudents; i++)
-	{
-		if (group.groupArr[i].grade >= minGrade)
-		{
+	for (int i = 0; i < group.amountOfStudents; i++) {
+		if (group.groupArr[i].grade >= minGrade) {
 			studentCounter++;
 		}
 	}
@@ -59,45 +47,35 @@ int numberOfStudentsTakingScholarship(StudentGroup& group, const double minGrade
 	return studentCounter;
 }
 
-void mySwap(Student& a, Student& b)
-{
+void mySwap(Student& a, Student& b) {
 	Student temp = a;
 	a = b;
 	b = temp;
 }
 
-void sortStudentsByGrade(Student* students, const int size)
-{
-	int min_idx;
+void sortStudentsByGrade(Student* students, const int size) {
+	for (int i = 0; i < size - 1; i++) {
+		size_t minIndex = i;
 
-	for (int i = 0; i < size - 1; i++)
-	{
-		min_idx = i;
-
-		for (int j = i + 1; j < size; j++)
-		{
-			if (students[j].grade < students[min_idx].grade)
-			{
-				min_idx = j;
+		for (int j = i + 1; j < size; j++) {
+			if (students[j].grade < students[minIndex].grade) {
+				minIndex = j;
 			}
 		}
 
-		if (min_idx != i)
-		{
-			mySwap(students[min_idx], students[i]);
+		if (minIndex != i) {
+			mySwap(students[minIndex], students[i]);
 		}
 	}
 
 }
 
-void printStudent(const Student& student)
-{
+void printStudent(const Student& student) {
 	std::cout << "Faculty number: " << student.FN << ", Name: " << student.name <<
 		", Grade: " << student.grade << std::endl;
 }
 
-bool isTakingScholarship(Student* students, const int pos, const double minGrade)
-{
+bool isTakingScholarship(const Student* students, const int pos, const double minGrade) {
 	return students[pos].grade >= minGrade;
 }
 
