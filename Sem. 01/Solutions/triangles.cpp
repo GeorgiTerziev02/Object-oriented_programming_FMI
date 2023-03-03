09ff8235c97014852283ede8b676c07b0183aa58
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-03-03 23:55:01 +0200

    finish triangles task

--- Sem. 01/Solutions/triangles.cpp
+++ Sem. 01/Solutions/triangles.cpp
@@ -8,6 +8,10 @@ void readPoint(Point& point) {
 	std::cin >> point.x >> point.y;
 }
 
+void printPoint(const Point& point) {
+	std::cout << point.x << " " << point.y;
+}
+
 double getSide(const Point& p1, const Point& p2) {
 	double dx = p1.x - p2.x;
 	double dy = p1.y - p2.y;
@@ -25,6 +29,15 @@ void readTriangle(Triangle& triangle) {
 	readPoint(triangle.p3);
 }
 
+void printTriangle(const Triangle& triangle) {
+	printPoint(triangle.p1);
+	std::cout << " ";
+	printPoint(triangle.p2);
+	std::cout << " ";
+	printPoint(triangle.p3);
+	std::cout << std::endl;
+}
+
 double getArea(const Triangle& triangle) {
 	double sideA = getSide(triangle.p1, triangle.p2);
 	double sideB = getSide(triangle.p2, triangle.p3);
@@ -36,31 +49,55 @@ double getArea(const Triangle& triangle) {
 	return sqrt(halfPerimeter * (halfPerimeter - sideA) * (halfPerimeter - sideB) * (halfPerimeter - sideC));
 }
 
-double* calculateAreas(const Triangle* triangles, int n) {
-	double* areas = new double[n];
+void swap(double& d1, double& d2) {
+	double temp = d1;
+	d1 = d2;
+	d2 = temp;
+}
 
-	for (int i = 0; i < n; i++) {
-		areas[i] = getArea(triangles[i]);
+void swap(Triangle& t1, Triangle& t2) {
+	Triangle temp = t1;
+	t1 = t2;
+	t2 = temp;
+}
+
+void sortTriangles(Triangle* triangles, double* areas, int size) {
+	for (int i = 0; i < size - 1; i++) {
+		int minElementIndex = i;
+
+		for (int j = i + 1; j < size; j++) {
+			if (areas[j] > areas[minElementIndex]) {
+				minElementIndex = j;
+			}
+		}
+
+		if (minElementIndex != i) {
+			swap(areas[i], areas[minElementIndex]);
+			swap(triangles[i], triangles[minElementIndex]);
+		}
 	}
-
-	return areas;
 }
 
 int main() {
-	int n;
-	std::cin >> n;
+	int size;
+	std::cin >> size;
 
-	Triangle* triangles = new Triangle[n];
+	Triangle* triangles = new Triangle[size];
+	double* areas = new double[size];
 
 	// read triangles
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < size; i++) {
 		readTriangle(triangles[i]);
+		areas[i] = getArea(triangles[i]);
 	}
 
-	double* areas = calculateAreas(triangles, n);;
-
-    // TODO: sort
-    // TODO: print
+    // sort triangles
+	sortTriangles(triangles, areas, size);
+	
+	// print triangles
+	for (int i = 0; i < size; i++) {
+		printTriangle(triangles[i]);
+	}
 
 	delete[] areas;
 	delete[] triangles;
