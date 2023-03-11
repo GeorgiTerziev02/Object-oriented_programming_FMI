7097be71b597b44752f62e2df3e9869f8a792005
kristinaganova <115446275+kristinaganova@users.noreply.github.com>
2023-03-11 18:28:38 +0200

    Pract.03: Add 01 - 02 | Kristina Ganova

--- Sem.03/task01.cpp
+++ Sem.03/task01.cpp
@@ -11,34 +11,14 @@ using std::ios;
 const char* FILE_NAME = "result.dat";
 const char* ERROR_MSG = "Error!";
 const size_t SIZE = 6;
+const char* OUTPUT_FILE_NAME = "output.txt";
+const size_t ADDRESS_SIZE = 25;
 
-struct Delivery {
+struct Delivery 
+{
 	double price;
-	char address[25];
+	char address[ADDRESS_SIZE + 1];
 };
-
-size_t getFileSize(ifstream& f)
-{
-	size_t currPos = f.tellg();
-	f.seekg(0, ios::end);
-	size_t size = f.tellg();
-	f.seekg(currPos, ios::beg);
-
-	return size;
-}
-
-void readFromFile(Delivery*& ptr, size_t& deliveriesCount, ifstream& f)
-{
-	size_t sizeOfFile = getFileSize(f);
-	deliveriesCount = sizeOfFile / sizeof(Delivery);
-	ptr = new Delivery[deliveriesCount];
-	f.read((char*)ptr, sizeOfFile);
-}
-
-void writeToFile(const Delivery* deliveries, size_t deliveriesCount, ofstream& f)
-{
-	f.write((const char*)deliveries, deliveriesCount * sizeof(Delivery));
-}
 
 int strCmp(const char* str1, const char* str2)
 {
@@ -50,11 +30,51 @@ int strCmp(const char* str1, const char* str2)
 	return *(const unsigned char*)str1 - *(const unsigned char*)str2; //ASCII difference
 }
 
+size_t getFileSize(ifstream& ifs)
+{
+	size_t currPos = ifs.tellg();
+	ifs.seekg(0, ios::end);
+	size_t size = ifs.tellg();
+	ifs.seekg(currPos, ios::beg);
+
+	return size;
+}
+
+void readFromFile(Delivery*& ptr, size_t& deliveriesCount, ifstream& ifs)
+{
+	size_t sizeOfFile = getFileSize(ifs);
+	deliveriesCount = sizeOfFile / sizeof(Delivery);
+	ptr = new Delivery[deliveriesCount];
+	ifs.read((char*)ptr, sizeOfFile);
+}
+
+void writeToFile(const Delivery* deliveries, size_t deliveriesCount, ofstream& ofs)
+{
+	ofs.write((const char*)deliveries, deliveriesCount * sizeof(Delivery));
+}
+
+void printDeliveries(const Delivery* arr, size_t count)
+{
+	for (size_t i = 0; i < count; i++)
+	{
+		cout << "Price: " << arr[i].price << "   " << "Address: " << arr[i].address << endl;
+	}
+}
+
+void saveDeliveriesToCVS(ofstream& out, const Delivery* deliveries, size_t deliveriesCount)
+{
+	out << "Price, Address" << endl;
+	for (size_t i = 0; i < deliveriesCount; i++)
+	{
+		out << deliveries[i].price << ", " << deliveries[i].address << endl;
+	}
+}
+
 void selectionSort(Delivery* delivery)
 {
 	for (size_t i = 0; i < SIZE - 1; i++)
 	{
-		int minIndex = i;
+		size_t minIndex = i;
 
 		for (size_t j = i + 1; j < SIZE; j++)
 		{
@@ -81,27 +101,57 @@ int main()
 	orders[4] = { 25.85, "Elk Avenue 2564" };
 	orders[5] = { 42.22, "Austin Avenue 1973" };
 
-	ifstream in(FILE_NAME,ios::in | ios::binary);
-	ofstream out(FILE_NAME, ios::out | ios::binary);
+	Delivery newDelivery = { 12.32, "Totion Street 4561" };
 
-	if (!in.is_open() || !out.is_open())
-	{
-		cout << ERROR_MSG << endl;
-		return -1;
-	}
-
-	writeToFile(orders,  sizeof(orders) / sizeof(Delivery), out);
-	out.close();
-
-    Delivery* arr;
+	Delivery* arr;
 	size_t count = 0;
-	readFromFile(arr, count, in);
-	selectionSort(arr);
 
-	for (size_t i = 0; i < count; i++)
 	{
-		cout << "price: " << arr[i].price  << "   " << "Address: " << arr[i].address << endl;
+		ofstream out(FILE_NAME, ios::out | ios::binary);
+
+		if (!out.is_open())
+		{
+			cout << ERROR_MSG << endl;
+			return -1;
+		}
+
+		writeToFile(orders, sizeof(orders) / sizeof(Delivery), out);
+
+		out.close();
 	}
 
-	in.close();
+	{
+		ifstream in(FILE_NAME, ios::in | ios::binary);
+		
+		if (!in.is_open())
+		{
+			cout << ERROR_MSG << endl;
+			return -1;
+		}
+
+		readFromFile(arr, count, in);
+
+		in.close();
+	}
+
+	{
+		ofstream output(OUTPUT_FILE_NAME);
+
+		if (!output.is_open())
+		{
+			cout << ERROR_MSG << endl;
+			return -1;
+		}
+
+		selectionSort(arr);
+
+		printDeliveries(arr, count);
+
+		saveDeliveriesToCVS(output, orders, count);
+
+		output.close();
+	}
+    
+	delete[] arr;
+	
 }
---
+++ Sem.03/task02.cpp
@@ -0,0 +1,285 @@
+#include <iostream>
+#include <fstream>
+#pragma warning(disable : 4996) 
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::ios;
+
+const int MAX_COMPANY_NAME_LEN = 25;
+const char* ERROR_MS = "Error!";
+const char* BINARY_FILE_NAME = "JobOffers.dat";
+const long long MIN_SALARY = 1234;
+const char* PERFECT_OFFERS = "perfectOffers.dat";
+
+namespace myFunctions
+{
+	int strCmp(const char* str1, const char* str2)
+	{
+		while (*str1 && (*str1 == *str2))// compares two strings character by character
+		{
+			str1++;
+			str2++;
+		}
+		return *(const unsigned char*)str1 - *(const unsigned char*)str2; //ASCII difference
+	}
+
+	int myStrlen(const char* str)
+	{
+		if (str == nullptr)
+		{
+			return 0;
+		}
+
+		int count = 0;
+
+		while (*str != '\0')
+		{
+			count++;
+			str++;
+		}
+
+		return count;
+	}
+
+	void myStrCopy(const char* source, char* dest)
+	{
+		if (source == nullptr || dest == nullptr)
+			return;
+
+		while (*source != '\0')
+		{
+			*dest = *source;
+			dest++;
+			source++;
+		}
+		*dest = '\0';
+	}
+}
+
+struct JobOffer
+{
+	char companyName[MAX_COMPANY_NAME_LEN + 1];
+	int coworkersCount;
+	int paidPaysOff;
+	long long projectSalary;
+};
+
+void initJobOffer(JobOffer& offer,const char* name, int coworkers, int daysOff, long long salary)
+{
+	myFunctions::myStrCopy(name, offer.companyName);
+	offer.coworkersCount = coworkers;
+	offer.paidPaysOff = daysOff;
+	offer.projectSalary = salary;
+}
+
+void saveToFile(const JobOffer& offer, ofstream& ofs)
+{
+	ofs.write((const char*)&offer, sizeof(offer));
+}
+
+void clearBinaryFile(const char* filePath) 
+{
+	ofstream ofs(filePath, ios::out | ios::binary | ios::trunc);
+	ofs.close();
+}
+
+size_t getFileSize(ifstream& ifs)
+{
+	size_t currPos = ifs.tellg();
+	ifs.seekg(0, ios::end);
+	size_t size = ifs.tellg();
+	ifs.seekg(currPos, ios::beg);
+
+	return size;
+}
+
+void readFromFile(ifstream& in, JobOffer& jobOffer)
+{
+	in.read((char*)&jobOffer, sizeof(jobOffer));
+}
+
+void filterOffers(const char* fileName, long long minSalary)
+{
+	JobOffer jobOffer = {};
+	ifstream in(fileName);
+
+	if (!in.is_open())
+	{
+		cout << ERROR_MS << endl;
+		return;
+	}
+
+	size_t fileSize = getFileSize(in);
+	size_t numberOfOrders = fileSize / sizeof(JobOffer);
+
+	for (size_t i = 0; i < numberOfOrders; i++)
+	{
+		in.read((char*)&jobOffer, sizeof(JobOffer));
+		if (jobOffer.projectSalary >= minSalary)
+		{
+			cout << "Company: " << jobOffer.companyName << " covers the minimum" << endl;
+		}
+	}
+	in.close();
+	
+}
+
+bool existOffer(const char* fileName, const char* companyName)
+{
+	JobOffer jobOffer;
+	ifstream in(fileName);
+
+	if (!in.is_open())
+	{
+		cout << ERROR_MS << endl;
+		return false;
+	}
+
+	size_t fileSize = getFileSize(in);
+	size_t numberOfOrders = fileSize / sizeof(JobOffer);
+
+	for (size_t i = 0; i < numberOfOrders; i++)
+	{
+		in.read((char*)&jobOffer, sizeof(JobOffer));
+		if (myFunctions::strCmp(jobOffer.companyName, companyName))
+		{
+			in.close();
+			return true;
+		}
+	}
+	in.close();
+	return false;
+}
+//Bonus 1
+void perfectOffer(const char* filePath, int maxCoworkers, int minVacancyDays, int minSalary) {
+	ifstream fileIn(filePath, ios::binary);
+
+	if (!fileIn.is_open()) {
+		cout << ERROR_MS << endl;
+		return;
+	}
+
+	ofstream fileOut(PERFECT_OFFERS, ios::binary);
+
+	if (!fileOut.is_open()) {
+		cout  << ERROR_MS << endl;
+		return;
+	}
+
+	JobOffer offer = {};
+
+	while (fileIn.read(reinterpret_cast<char*>(&offer), sizeof(JobOffer))) {
+		if (offer.coworkersCount == maxCoworkers && offer.paidPaysOff == minVacancyDays && offer.projectSalary == minSalary) {
+			fileOut.write((const char*)(&offer), sizeof(JobOffer));
+		}
+	}
+
+	fileIn.close();
+	fileOut.close();
+}
+//Bonus2
+void addJobOffers(JobOffer* jobOffers, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Job offer: " << i + 1 << endl << "Enter company name: " << endl;
+		cin.getline(jobOffers[i].companyName, MAX_COMPANY_NAME_LEN + 1);
+		cout << "Enter coworkers count: " << endl;
+		cin >> jobOffers[i].coworkersCount;
+		cout << "Enter paid leave days: " << endl;
+		cin >> jobOffers[i].paidPaysOff;
+		cout << "Enter project pay: " << endl;
+		cin >> jobOffers[i].projectSalary;
+
+		cin.ignore();
+	}
+}
+
+void displayAllOffers(const char* filePath)
+{
+	ifstream in(filePath, ios::in | ios::binary);
+	if (!in.is_open())
+	{
+		cout << ERROR_MS << endl;
+		return;
+	}
+
+	JobOffer offer;
+	while (in.read((char*)&offer, sizeof(offer)))
+	{
+		cout << "Company name: " << offer.companyName << endl;
+		cout << "Number of coworkers: " << offer.coworkersCount << endl;
+		cout << "Number of vacation days: " << offer.paidPaysOff << endl;
+		cout << "Salary: $" << offer.projectSalary << endl;
+		cout << endl;
+	}
+	in.close();
+}
+
+int main() 
+{
+	JobOffer jobOffers[10];
+	int n = 0;
+
+	{
+		cout << "How many job offers would you like to add?" << endl;
+		cin >> n;
+
+		cin.ignore();
+
+		addJobOffers(jobOffers, n);
+	}
+
+	{
+		ofstream ofs(BINARY_FILE_NAME, ios::out | ios::binary | ios::app);
+		if (!ofs.is_open()) {
+			cout << ERROR_MS << endl;
+			return 1;
+		}
+
+		for (int i = 0; i < n; i++) {
+			saveToFile(jobOffers[i], ofs);
+		}
+
+		ofs.close();
+	}
+
+	displayAllOffers(BINARY_FILE_NAME);
+
+	{
+		long long minSalary = 0;
+
+		cout << "Enter minimum salary: " << endl;
+		cin >> minSalary;
+
+		filterOffers(BINARY_FILE_NAME, minSalary);
+	}
+
+	{
+		cout << "Enter company name: " << endl;
+		char companyName[26];
+		cin.ignore();
+		cin.getline(companyName, 26);
+
+		if (existOffer(BINARY_FILE_NAME, companyName)) {
+			cout << companyName << " exists" << endl;
+		}
+		else {
+			cout << companyName << " does not exist" << endl;
+		}
+	}
+
+	perfectOffer(BINARY_FILE_NAME, 5, 25, 1234);
+
+	return 0;
+
+	//clearBinaryFile(BINARY_FILE_NAME);
+
+	
+}
+
+
