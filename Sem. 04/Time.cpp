5aa1f4ac0066e1e1a1aabecc0fa06f9a68a37c5c
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-03-16 10:06:07 +0200

    add main

--- Sem. 04/Time.cpp
+++ Sem. 04/Time.cpp
@@ -106,6 +106,7 @@ public:
 	bool isPartyTime() const {
 		Time lowerBound(23, 0, 0);
 		Time upperBound(6, 0, 0);
+
 		return compare(lowerBound) >= 0 || compare(upperBound) <= 0;
 	}
 
@@ -120,4 +121,14 @@ public:
 
 void stableSortTimes(Time* arr, size_t arrSize) {
 	// TODO:
+}
+
+int main()
+{
+	Time times[10] = { Time(11, 30, 0), Time(12, 44, 29), Time(), Time(17, 15, 23), Time(23, 60, 0), Time(3, 14, 15), Time(2, 71, 82), Time(21, 22, 3), Time(17, 15, 23), Time(7, 31, 43) };
+	stableSortTimes(times, 10);
+	
+	for (int i = 0; i < 10; i++) {
+		times[i].print();
+	}
 }
