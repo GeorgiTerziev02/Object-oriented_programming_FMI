51e8e58ad906efc3f58ca222fba75f08791f5ec7
Radopeev <95441540+Radopeev@users.noreply.github.com>
2023-03-17 21:16:19 +0200

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 #pragma warning(disable : 4996)
+
+unsigned int strLen(const char* s)
+{
+    unsigned int count = 0;
+    while (*s != '\0')
+    {
+        count++;
+        s++;
+    }
+    return count;
+}
+
+char* strCopy(char* destination, const char* source) {
+    if (destination == nullptr) {
+        return NULL;
+    }
+
+    char *ptr = destination;
+
+    while (*source != '\0') {
+        *destination = *source;
+        destination++;
+        source++;
+    }
+    *destination = '\0';
+    return ptr;
+}
+
 class Teacher {
 private:
     char* name;
@@ -8,11 +36,13 @@ private:
     int expirience;
 public:
     Teacher() : age(0),expirience(0) {
+        name = new char[1];
+        name[0] = '\0';
     }
-    Teacher(const char* name,int namelength,int age,int expirience) {
-        this->namelength=namelength;
-        this->name = new char[namelength + 1];
-        strcpy(this->name, name);
+    Teacher(const char* name,int age,int expirience) {
+        this->namelength = strLen(name);
+        this->name = new char[this->namelength + 1];
+        strCopy(this->name, name);
         this->age = age;
         this->expirience=expirience;
     }
@@ -33,11 +63,11 @@ public:
         return this->expirience;
     }
 
-    void setName(char* name,int nameLength) {
+    void setName(const char* name) {
         delete[] this->name;
-        this->namelength = namelength;
-        this->name = new char[namelength + 1];
-        strcpy(this->name, name);
+        this->namelength = strLen(name);
+        this->name = new char[this->namelength + 1];
+        strCopy(this->name, name);
     }
 
     void setAge(int age) {
@@ -56,9 +86,9 @@ public:
 
 int main()
 {
-    Teacher teacher("Gospodinov", 11, 58, 30); 
+    Teacher teacher("Gospodinov", 58, 30); 
     char* teacherName = new char[teacher.getNameLength()+1];
-    strcpy(teacherName, teacher.getName());
+    strCopy(teacherName, teacher.getName());
     std::cout << "Teacher:" << std::endl;
     std::cout << teacher.getName() << " " << teacher.getAge() << " " << teacher.getExp();
 
