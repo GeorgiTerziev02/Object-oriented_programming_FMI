f25a744dc4df50126e048a80f587a8412749b45a
Radopeev <95441540+Radopeev@users.noreply.github.com>
2023-03-17 21:16:22 +0200

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <fstream>
 #pragma warning(disable : 4996)
+
+char* strCopy(char* destination, const char* source) {
+    if (destination == nullptr) {
+        return NULL;
+    }
+    char* ptr = destination;
+    while(*source!='\0'){
+        *destination = *source;
+        destination++;
+        source++;
+    }
+    *destination = '\0';
+    return ptr;
+}
+
+size_t strLen(const char* s) {
+    size_t count = 0;
+    while (*s != '\0') {
+        count++;
+        s++;
+    }
+    return count;
+}
+
 const int MAX_TASKS = 5;
-const int MAX = 0;
+
 class Task {
 private:
     char* name;
-    int nameLength;
+    size_t nameLength;
     char* requirement;
-    int requirementLength;
+    size_t requirementLength;
     int points;
 
 public:
     Task() = default;
-    Task(const char* name, int nameLength ,const  char* requirement,int requirementLength,int point) {
-        this->nameLength = nameLength;
-        this->name = new char[nameLength + 1];
-        strcpy(this->name, name);
-        this->requirementLength = requirementLength;
-        this->requirement = new char[requirementLength + 1];
-        strcpy(this->requirement, requirement);
+    Task(const char* name,const  char* requirement,int point) {
+        this->nameLength = strLen(name);
+        this->name = new char[this->nameLength + 1];
+        strCopy(this->name, name);
+        this->requirementLength = strLen(requirement);
+        this->requirement = new char[this->requirementLength + 1];
+        strCopy(this->requirement, requirement);
         this->points=points;
     }
 
@@ -43,22 +67,22 @@ public:
         return this->points;
     }
 
-    void setName(char* name, int nameLength) {
+    void setName(const char* name) {
         if (name != nullptr) {
             delete[] this->name;
         }
-        this->nameLength = nameLength;
-        this->name = new char[nameLength + 1];
-        strcpy(this->name, name);
+        this->nameLength = strLen(name);
+        this->name = new char[this->nameLength + 1];
+        strCopy(this->name, name);
     }
     
-    void setRequirement(char* requirement, int requirementLength) {
+    void setRequirement(char* requirement) {
         if (requirement != nullptr) {
             delete[] this->requirement;
         }
-        this->requirementLength = requirementLength;
-        this->requirement = new char[requirementLength + 1];
-        strcpy(this->name, name);
+        this->requirementLength = strLen(requirement);
+        this->requirement = new char[this->requirementLength + 1];
+        strCopy(this->name, name);
     }
 
     void setPoints(int points) {
@@ -69,22 +93,33 @@ public:
         if (!out.is_open()) {
             return;
         }
-        out.write((const char*)this->getNameLength(), sizeof(this->getNameLength()));
-        out.write((const char*)this->getName(), this->getNameLength());
-        out.write((const char*)this->getRequirementLength(), sizeof(this->getRequirementLength()));
-        out.write((const char*)this->getRequirement(), this->getRequirementLength());
-        out.write((const char*)this->getPoints(),this->getPoints());
+        out.write((const char*)this->nameLength, sizeof(this->nameLength));
+        out.write((const char*)this->name, this->nameLength);
+        out.write((const char*)this->requirementLength, sizeof(this->requirementLength));
+        out.write((const char*)this->requirement, this->requirementLength);
+        out.write((const char*)this->points,sizeof(this->points));
     }
 
     void readTaskToFile(std::ifstream& inputFile) const{
         if (!inputFile.is_open()) {
             return;
         }
-        inputFile.read((char*)this->getNameLength(), sizeof(getNameLength()));
-        inputFile.read((char*)this->getName(), this->getNameLength());
-        inputFile.read((char*)this->getRequirementLength(), sizeof(getRequirementLength()));
-        inputFile.read((char*)this->getRequirement(), this->getRequirementLength());
-        inputFile.read((char*)this->getPoints(), sizeof(this->getPoints()));
+
+        if (this->name != nullptr) {
+            delete[] this->name;
+        }
+
+        if (this->requirement != nullptr) {
+            delete[] this->requirement;
+        }
+
+
+
+        inputFile.read((char*)this->nameLength, sizeof(this->nameLength));
+        inputFile.read((char*)this->name, this->nameLength);
+        inputFile.read((char*)this->requirementLength, sizeof(this->requirementLength));
+        inputFile.read((char*)this->requirement, this->requirementLength);
+        inputFile.read((char*)this->points, sizeof(this->points));
     }
 
     ~Task()
@@ -104,9 +139,14 @@ public:
 
     void writeToFile(const char* fileName)const {
         std::ofstream out(fileName, std::ios::binary);
+        
         if (!out.is_open()) {
             return;
         }
+
+        out.write((const char*)this->numberOfTasks, sizeof(this->numberOfTasks));
+        out.write((const char*)this->minPoints, sizeof(this->minPoints));
+
         for (int i = 0; i < this->numberOfTasks; i++) {
             this->tasks[i].writeTaskToFile(out);
         }
@@ -118,6 +158,10 @@ public:
         if (!inputFile.is_open()) {
             return;
         }
+
+        inputFile.read((char*)this->numberOfTasks, sizeof(this->numberOfTasks));
+        inputFile.read((char*)this->minPoints, sizeof(this->minPoints));
+
         for (int i = 0; i < this->numberOfTasks; i++) {
             this->tasks[i].readTaskToFile(inputFile);
         }
@@ -128,10 +172,10 @@ public:
         this->minPoints = newMinPoints;
     }
     
-    int getMax() {
-        int max = MAX;
+    int getMax() const {
+        int max = 0;
         for (int i = 0; i < numberOfTasks; i++) {
-            if (this->tasks[i].getPoints() > MAX) {
+            if (this->tasks[i].getPoints() > max) {
                 max = this->tasks[i].getPoints();
             }
         }
