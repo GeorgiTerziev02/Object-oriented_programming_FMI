81226f8fde759903f6da39c4ce816953e89395fa
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-03-22 14:35:39 +0200

    Update Time.cpp

--- Sem. 04/Solutions/Time.cpp
+++ Sem. 04/Solutions/Time.cpp
@@ -107,7 +107,7 @@ int Time::compare(const Time& other) const {
 	unsigned currentTotalSeconds = getTotalSeconds();
 	unsigned otherTotalSeconds = other.getTotalSeconds();
 
-	if(currentTotalSeconds == otherTotalSeconds) {
+	if (currentTotalSeconds == otherTotalSeconds) {
 		return 0;
 	}
 
@@ -125,7 +125,7 @@ Time Time::getTimeToMidnight() const {
 bool Time::isDinnerTime() const {
 	Time lowerBound(20, 30, 0);
 	Time upperBound(22, 0, 0);
-	
+
 	return compare(lowerBound) >= 0 && compare(upperBound) <= 0;
 }
 
@@ -140,18 +140,28 @@ unsigned Time::getTotalSeconds() const {
 }
 
 void Time::print() const {
-	std::cout << hours << ":" << minutes << ":" << seconds;
+	std::cout << hours << ":" << minutes << ":" << seconds << std::endl;
 }
 
 void stableSortTimes(Time* arr, size_t arrSize) {
-	// TODO:
+	for (int i = 1; i < arrSize; i++) {
+		Time temp = arr[i];
+
+		int j = i - 1;
+		while (j >= 0 && arr[j].compare(temp) == 1) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+
+		arr[j + 1] = temp;
+	}
 }
 
 int main()
 {
 	Time times[10] = { Time(11, 30, 0), Time(12, 44, 29), Time(), Time(17, 15, 23), Time(23, 60, 0), Time(3, 14, 15), Time(2, 71, 82), Time(21, 22, 3), Time(17, 15, 23), Time(7, 31, 43) };
 	stableSortTimes(times, 10);
-	
+
 	for (int i = 0; i < 10; i++) {
 		times[i].print();
 	}
