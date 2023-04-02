540a23e581b4133273fc9a20f128c9a57e5ff102
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-04-02 16:26:07 +0300

    Update temp-getLinesCount.cpp

--- Sem. 02/Solutions/temp-getLinesCount.cpp
+++ Sem. 02/Solutions/temp-getLinesCount.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
 #include <fstream>
-#include "constants.h"
 
-// TODO: refactor
-size_t getLinesCount(std::ifstream& in) {
-	size_t counter = 0;
-	while (!in.eof()) {
-		char buffer[BUFFER_SIZE];
-		in.getline(buffer, BUFFER_SIZE, '\n');
-		counter++;
+unsigned getCharCountFromFile(std::ifstream& ifs, char ch) {
+	size_t currentPosition = ifs.tellg();
+	ifs.seekg(0, std::ios::beg);
+
+	if (!ifs.is_open()) {
+		return 0;
 	}
 
-	return counter;
+	unsigned int count = 0;
+	char current;
+	while (true) {
+		in.get(current); // current = in.get();
+		if (in.eof()) {
+			break;
+		}
+
+		if (current == ch) {
+			count++;
+		}
+	}
+
+	in.clear();
+	in.seekg(currentPosition);
+	return count;
+}
+
+unsigned getLinesCount(const char* fileName) {
+	std::ifstream in(fileName);
+
+	if (!in.is_open()) {
+		return 0;
+	}
+
+	return getCharCountFromFile(in, '\n') + 1;
+}
+
+int main()
+{
+	std::cout << getLinesCount("file.txt");
 }
