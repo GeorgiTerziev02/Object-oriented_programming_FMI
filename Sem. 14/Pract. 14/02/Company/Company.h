11fde349c511fdb69f772d444ddad0135e9f89e8
kristinaganova <115446275+kristinaganova@users.noreply.github.com>
2023-06-04 00:03:07 +0300

    Update Company.h

--- Sem. 14/Pract. 14/02/Company/Company.h
+++ Sem. 14/Pract. 14/02/Company/Company.h
@@ -7,38 +7,61 @@ class Company
 private:
 	Vector<Employee*> employees;
 
+	void copyFrom(const Company& other);
+	void moveFrom(Company&& other);
+	void free();
+
 public:
 	~Company();
+	Company();
+	Company(const Company& other);
+	Company& operator=(const Company& other);
+	Company(Company&& other) noexcept;
+	Company& operator=(Company&& other) noexcept;
 
-	void addEmployee(Employee* employee);
-	void addWorker(Employee* worker);
-	void addTrainee(Employee* trainee);
-	void addPaidTrainee(Employee* paidTrainee);
 
 	class Manager : public Employee
 	{
 		Vector<Employee*> subordinates;
+
+		void copyFrom(const Manager& other);
+		void moveFrom(Manager&& other);
+		void free();
 	public:
 		Manager(const MyString& name, unsigned int age, double salary, Vector<Employee*> subordinates);
+		Manager(const Manager& other);
+		Manager& operator=(const Manager& other);
+		Manager(Manager&& other) noexcept;
+		Manager& operator=(Manager&& other) noexcept;
+		~Manager();
+
 		void addSubordinate(Employee* subordinate);
 		void print() const override;
 		Employee* clone() const override;
-		~Manager();
 	};
-
 
 	class CEO : public Employee
 	{
 		Vector<Manager*> managers;
+
+		void copyFrom(const CEO& other);
+		void moveFrom(CEO&& other);
+		void free();
 	public:
-		CEO(const MyString& name, unsigned int age, double salary, Vector<Manager*> subordinates);
 		CEO();
+		CEO(const MyString& name, unsigned int age, double salary, Vector<Manager*> subordinates);
+		CEO(const CEO& other);
+		CEO& operator=(const CEO& other);
+		CEO(CEO&& other) noexcept;
+		CEO& operator=(CEO&& other) noexcept;
+		~CEO();
+
 		void addSubordinate(Manager* subordinate);
 		void print() const override;
 	    Employee* clone() const override;
-		~CEO();
 	};
 
+	void addEmployee(Employee* employee);
 	double calculateAverageSalary() const;
 	bool hasTwoEmployeesWithSumSalary(double val) const;
 
