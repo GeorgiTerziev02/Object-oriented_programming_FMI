078de888fcc04e6fac3b9e7005876b436f2ea7ee
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-06-10 16:19:46 +0300

    Update Command.h

--- Sem. 15/Design Patterns/Behavioural/Command/Command.h
+++ Sem. 15/Design Patterns/Behavioural/Command/Command.h
@@ -1,31 +1,119 @@
 #pragma once
 
-// TODO: finish
-//class Command
-//{
-//public:
-//	virtual void execute() const = 0;
-//	virtual ~Command() = default;
-//};
-//
-//class Entity {};
-//
-//// Receiver
-//class Database {
-//public:
-//	void save(const Entity& entity) {
-//		//...
-//	}
-//};
-//
-//class SaveEntityToDatabaseCommand : public Command {
-//	Database* db;
-//	Entity* entity;
-//public:
-//	SaveEntityToDatabaseCommand(Database* db, Entity* entity)
-//		: db(db), entity(entity) { }
-//
-//	void execute() const override {
-//		db->save(*entity);
-//	}
-//};
+#include <string>
+
+class Command {
+public:
+  virtual ~Command() = default;
+  virtual void execute() const = 0;
+};
+
+class SimpleCommand : public Command {
+private:
+  std::string data;
+public:
+  explicit SimpleCommand(std::string data) : data(data) { }
+  
+  void execute() const override {
+    std::cout << "SimpleCommand: See, I can do simple things like printing (" << data << ")\n";
+  }
+};
+
+/**
+ * The Receiver classes contain some important business logic. They know how to
+ * perform all kinds of operations, associated with carrying out a request. In
+ * fact, any class may serve as a Receiver.
+ */
+class Receiver {
+ public:
+  void doSomething(const std::string& a) {
+    std::cout << "Receiver: Working on (" << a << ".)\n";
+  }
+  void doSomethingElse(const std::string& b) {
+    std::cout << "Receiver: Also working on (" << b << ".)\n";
+  }
+};
+
+/**
+ * However, some commands can delegate more complex operations to other objects, called "receivers."
+ */
+class ComplexCommand : public Command {
+private:
+  Receiver *receiver_;
+  /**
+   * Context data, required for launching the receiver's methods.
+   */
+  std::string a_;
+  std::string b_;
+  /**
+   * Complex commands can accept one or several receiver objects along with any
+   * context data via the constructor.
+   */
+ public:
+  ComplexCommand(Receiver *receiver, std::string a, std::string b) : receiver_(receiver), a_(a), b_(b) {
+  }
+  /**
+   * Commands can delegate to any methods of a receiver.
+   */
+  void execute() const override {
+    std::cout << "ComplexCommand: Complex stuff should be done by a receiver object.\n";
+    this->receiver_->DoSomething(this->a_);
+    this->receiver_->DoSomethingElse(this->b_);
+  }
+};
+
+/**
+ * The Invoker is associated with one or several commands. It sends a request to
+ * the command.
+ */
+class Invoker {
+  /**
+   * @var Command
+   */
+ private:
+  Command *on_start_;
+  /**
+   * @var Command
+   */
+  Command *on_finish_;
+  /**
+   * Initialize commands.
+   */
+ public:
+  ~Invoker() {
+    delete on_start_;
+    delete on_finish_;
+  }
+
+  void SetOnStart(Command *command) {
+    this->on_start_ = command;
+  }
+  void SetOnFinish(Command *command) {
+    this->on_finish_ = command;
+  }
+  
+  void DoSomethingImportant() {
+    std::cout << "Invoker: Does anybody want something done before I begin?\n";
+    if (this->on_start_) {
+      this->on_start_->execute();
+    }
+    std::cout << "Invoker: ...doing something really important...\n";
+    std::cout << "Invoker: Does anybody want something done after I finish?\n";
+    if (this->on_finish_) {
+      this->on_finish_->execute();
+    }
+  }
+};
+
+int testFunc() {
+  Invoker *invoker = new Invoker;
+  invoker->SetOnStart(new SimpleCommand("Say Hi!"));
+  Receiver *receiver = new Receiver;
+  invoker->SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
+  invoker->DoSomethingImportant();
+
+  delete invoker;
+  delete receiver;
+
+  return 0;
+}
