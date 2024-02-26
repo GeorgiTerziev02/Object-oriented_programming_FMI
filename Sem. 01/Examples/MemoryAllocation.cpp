008f57eaa38cd9ac9d52152ff7880f4a68cd8b73
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-02-26 11:55:46 +0200

    Update MemoryAllocation.cpp

--- Sem. 01/Examples/MemoryAllocation.cpp
+++ Sem. 01/Examples/MemoryAllocation.cpp
@@ -4,12 +4,12 @@ int main() {
 	// How much memory is allocated in the following expressions and where?
 	// We assume that our os is x32 and the pointer takes 4 bytes
 	short s = 1;				// Stack:	2	Heap:	0
-	int a = 2;				    // Stack:	4	Heap:	0
+	int a = 2;				// Stack:	4	Heap:	0
 	bool b = true;				// Stack:	1	Heap:	0
 	bool* p = new bool;			// Stack:	4	Heap:   1
 	char c = '\0';				// Stack:	1	Heap:	0
 	double d = 11.1;			// Stack:	8	Heap:	0
-	int arr1[40] = { 0 };		// Stack:	160	Heap:	0
+	int arr1[40] = { 0 };		        // Stack:	160	Heap:	0
 	// arr1 is array in the stack => 40 * 4 bytes
 	
 	int* arr2 = new int(40);		// Stack:	4	Heap:   4
@@ -19,7 +19,7 @@ int main() {
 	for (size_t i = 1; i < 10; i++) {
 		matrix[i] = new int[i]; // new int(i) vs new int[i]
 	}
-    // line 18 to 21 in two situations
+        // line 18 to 21 in two situations
 	// 1) new int(i)
 	// Stack:  4    Heap: 10 * 4 + 9 * 4
 	// 2) new int[i]
