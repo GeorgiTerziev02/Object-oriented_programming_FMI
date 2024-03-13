6475ac7cdd973627c4f42e673c5a6ac5d265d6a6
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-03-14 01:06:56 +0200

    add initial part of interval and time

---
+++ Sem. 04/Solutions/Interval.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+class Interval {
+private:
+    int start;
+    int end;
+
+public:
+    Interval() : Interval(0, 0) { }
+
+    Interval(int start, int end) : start(start), end(end) {
+        if(start > end) {
+            this->start = this->end = 0;
+        }
+    }
+
+    int getStart() const {
+        return start;
+    }
+
+    int getEnd() const {
+        return end;
+    }
+
+    void setStart(int newValue) {
+        if(newValue > end) {
+            return;
+        }
+        start = newValue;
+    }
+
+    void setEnd(int newValue) {
+        if(newValue < start) {
+            return;
+        }
+        end = newValue;
+    }
+
+    unsigned getLength() const {
+        return end - start + 1;
+    }
+
+    bool isInInterval(int value) const {
+        return start <= value && value <= end;
+    }
+
+    void print() const {
+        std::cout << start << " - " << end << std::endl;
+    }
+};
