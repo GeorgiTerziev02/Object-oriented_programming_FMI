27d5f1c7022c894401b8652865e8477ccec28fc0
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-03-21 17:30:53 +0200

    add explicit example

---
+++ Sem. 05/Example/ExplicitConstructor/main.cpp
@@ -0,0 +1,16 @@
+class ExplicitConstructor {
+public:
+    explicit ExplicitConstructor(int a) { }
+};
+
+class ImplicitConstructor {
+public:
+    ImplicitConstructor(int a) { }
+};
+
+int main() {
+    ExplicitConstructor ec = 5; // error: cannot convert 'int' to 'ExplicitConstructor' in initialization
+    ExplicitConstructor ec2 = ExplicitConstructor(5);
+    ExplicitConstructor ec3(5);
+    ImplicitConstructor ic = 5; // OK
+}
