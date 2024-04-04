b95e70373823c817b2e3392e17cbc4962ac0f409
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-04-04 17:21:16 +0300

    refactor

--- Sem. 06/Solutions/Bitsets/Dynamic-Bitset/Bitset.cpp
+++ Sem. 06/Solutions/Bitsets/Dynamic-Bitset/Bitset.cpp
@@ -1,59 +1,49 @@
 #include "Bitset.h"
 #include <iostream>
 
-void Bitset::copyFrom(const Bitset& other)
-{
-	maxNum = other.maxNum;
+namespace {
+	constexpr size_t BUCKET_SIZE = 8;
 
-	unsigned int bucketsCount = getBucketsCount();
+	unsigned char getMask(unsigned int n) {
+		unsigned int shiftIndex = (n % BUCKET_SIZE);
+
+		unsigned char mask = 1;
+		mask <<= (BUCKET_SIZE - 1 - shiftIndex);
+		return mask;
+	}
+
+	unsigned int getBucketIndex(unsigned int n) {
+		return n / BUCKET_SIZE;
+	}
+}
+
+void Bitset::copyFrom(const Bitset& other) {
+	maxNum = other.maxNum;
+	bucketsCount = other.bucketsCount;
 	data = new unsigned char[bucketsCount];
-	for (size_t i = 0; i < bucketsCount; i++)
-	{
+	for (size_t i = 0; i < bucketsCount; i++) {
 		data[i] = other.data[i];
 	}
 }
 
-void Bitset::free()
-{
+void Bitset::free() {
 	delete[] data;
 	data = nullptr;
-	maxNum = 0;
+	maxNum = bucketsCount = 0;
 }
 
-unsigned int Bitset::getBucketIndex(unsigned int n) const
-{
-	return n / BUCKET_SIZE;
-}
-
-unsigned int Bitset::getBucketsCount() const
-{
-	return maxNum / BUCKET_SIZE + 1;
-}
-
-unsigned char Bitset::getMask(unsigned int n) const
-{
-	unsigned int shiftIndex = (n % BUCKET_SIZE);
-
-	unsigned char mask = 1;
-	mask <<= (BUCKET_SIZE - 1 - shiftIndex);
-	return mask;
-}
-
-Bitset::Bitset(unsigned int max)
-{
+Bitset::Bitset(unsigned int max) {
 	maxNum = max;
-	data = new unsigned char[getBucketsCount()]{};
+	bucketsCount = max / BUCKET_SIZE + 1;
+	data = new unsigned char[bucketsCount]{};
 }
 
-Bitset::Bitset(const Bitset& other)
-{
+Bitset::Bitset(const Bitset& other) {
 	copyFrom(other);
 }
 
-Bitset& Bitset::operator=(const Bitset& other)
-{
-	if (this != &other)
-	{
+Bitset& Bitset::operator=(const Bitset& other) {
+	if (this != &other) {
 		free();
 		copyFrom(other);
 	}
@@ -61,13 +51,11 @@ Bitset& Bitset::operator=(const Bitset& other)
 	return *this;
 }
 
-Bitset::~Bitset()
-{
+Bitset::~Bitset() {
 	free();
 }
 
-void Bitset::add(unsigned int n)
-{
+void Bitset::add(unsigned int n) {
 	if (n > maxNum) {
 		return;
 	}
@@ -78,8 +66,7 @@ void Bitset::add(unsigned int n)
 	data[bucket] |= mask;
 }
 
-void Bitset::remove(unsigned int n)
-{
+void Bitset::remove(unsigned int n) {
 	if (n > maxNum) {
 		return;
 	}
@@ -90,8 +77,7 @@ void Bitset::remove(unsigned int n)
 	data[bucket] &= ~mask;
 }
 
-bool Bitset::contains(unsigned int n) const
-{
+bool Bitset::contains(unsigned int n) const {
 	if (n > maxNum) {
 		return false;
 	}
@@ -102,10 +88,8 @@ bool Bitset::contains(unsigned int n) const
 	return (data[bucket] & mask);
 }
 
-void Bitset::print() const
-{
-	for (size_t i = 0; i <= maxNum; i++)
-	{
+void Bitset::print() const {
+	for (size_t i = 0; i <= maxNum; i++) {
 		if (contains(i)) {
 			std::cout << i << ' ';
 		}
@@ -114,20 +98,17 @@ void Bitset::print() const
 	std::cout << std::endl;
 }
 
-Bitset unionOfSets(const Bitset& lhs, const Bitset& rhs)
-{
+Bitset unionOfSets(const Bitset& lhs, const Bitset& rhs) {
 	Bitset result(std::max(lhs.maxNum, rhs.maxNum));
-	unsigned int commonBucketsCount = std::min(lhs.getBucketsCount(), rhs.getBucketsCount());
+	unsigned int commonBucketsCount = std::min(lhs.bucketsCount, rhs.bucketsCount);
 
-	for (size_t i = 0; i < commonBucketsCount; i++)
-	{
+	for (size_t i = 0; i < commonBucketsCount; i++) {
 		result.data[i] = lhs.data[i] | rhs.data[i];
 	}
 
 	const Bitset& largerSet = lhs.maxNum > rhs.maxNum ? lhs : rhs;
-	unsigned int largerSetBuckets = largerSet.getBucketsCount();
-	for (size_t i = commonBucketsCount; i < largerSetBuckets; i++)
-	{
+	unsigned int largerSetBuckets = largerSet.bucketsCount;
+	for (size_t i = commonBucketsCount; i < largerSetBuckets; i++) {
 		result.data[i] = largerSet.data[i];
 	}
 
@@ -135,13 +116,11 @@ Bitset unionOfSets(const Bitset& lhs, const Bitset& rhs)
 
 }
 
-Bitset intersectionOfSets(const Bitset& lhs, const Bitset& rhs)
-{
+Bitset intersectionOfSets(const Bitset& lhs, const Bitset& rhs) {
 	Bitset result(std::min(lhs.maxNum, rhs.maxNum));
-	unsigned int commonBucketsCount = std::min(lhs.getBucketsCount(), rhs.getBucketsCount());
+	unsigned int commonBucketsCount = std::min(lhs.bucketsCount, rhs.bucketsCount);
 
-	for (size_t i = 0; i < commonBucketsCount; i++)
-	{
+	for (size_t i = 0; i < commonBucketsCount; i++) {
 		result.data[i] = lhs.data[i] & rhs.data[i];
 	}
 
