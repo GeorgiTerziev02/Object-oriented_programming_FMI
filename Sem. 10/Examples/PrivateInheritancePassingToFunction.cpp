1f3bcc5d13f6486497960569711541dd037038bb
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2024-04-24 18:45:51 +0300

    add private inheritance example

---
+++ Sem. 10/Examples/PrivateInheritancePassingToFunction.cpp
@@ -0,0 +1,14 @@
+class Base {
+public:
+    int x;
+};
+
+class Derived : private Base { };
+
+void f(Base& b) { }
+
+int main() {
+    Derived d;
+    f(d); // Compilation error
+    // Conversion from Derived to Base is inaccessible
+}
